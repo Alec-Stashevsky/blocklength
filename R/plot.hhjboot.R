ef33d4d0bdf51ac5e3646f062ae2701c43844db4
Alec <alec.stashevsky@protonmail.com>
2021-02-08 20:49:52 -0800

    pass checks w/ plot.hhjboot

--- R/plot.hhjboot.R
+++ R/plot.hhjboot.R
@@ -1,12 +1,25 @@
 #' Plot MSE Function for HHJ Algorithm
 #'
 #' @param x an object of class "hhjboot"
-#' @param ... Arguments to be passed to methods
 #' @param iter a vector of \code{hhjboot()} iterations to plot. \pkg{NULL}, by
 #'  default will plot all iterations
+#' @param ... Arguments to be passed to methods
 #'
 #' @export
-plot.hhjboot <- function(x, ..., iter = NULL) {
+#' @examples
+#' \dontrun{
+#' # Generate AR(1) time series
+#' sim <- stats::arima.sim(list(order = c(1, 0, 0), ar = 0.5),
+#'                         n = 500, innov = rnorm(500))
+#'
+#' # Generate 'hhjboot' class object of optimal block length for series
+#' hhj <- hhjboot(sim, sub.block.size = 10)
+#'
+#' ## S3 method for class 'hhjboot'
+#' plot(hhj)
+#' }
+#'
+plot.hhjboot <- function(x, iter = NULL, ...) {
 
   if (!inherits(x, "hhjboot")) {
     stop("use only with \"hhjboot\" objects")
@@ -26,7 +39,7 @@ plot.hhjboot <- function(x, ..., iter = NULL) {
     # Filter on iteration
     p.data <- x$`MSE Data`[x$`MSE Data`$Iteration == i, ]
 
-    plot.default(
+    graphics::plot.default(
       x = as.vector(p.data$BlockLength, mode = "numeric"),
       y = p.data$MSE,
       main = paste0(
