a0d239334e3de0447aa55ffe9159176aebd72447
Alec <alec.stashevsky@protonmail.com>
2021-02-20 15:27:34 -0800

    remove lagacy comments in pwsd

--- R/pwsd.R
+++ R/pwsd.R
@@ -76,7 +76,7 @@ pwsd <- function(
   correlogram = TRUE
   ){
 
-  # Set PW parameters and coerce to dataframe
+  # Set PW parameters and coerce to data.frame
   data <- data.frame(data)
   n <- nrow(data)
   k <- ncol(data)
@@ -84,35 +84,22 @@ pwsd <- function(
   # Save function call
   call <- match.call()
 
-  ## Set Defaults. Note that in footnote c, page 59, for K_N Politis
-  ## and White (2004) use max(5,log10(n)). Since this must be an
-  ## integer we use ceiling(log10(n)).
-
+  # Set defaults pilot parameters per footnote c, page 59, Politis and White (2004)
   if (is.null(K_N)) K_N <- max(5, ceiling(log10(n)))
   if (is.null(M_max)) M_max <- ceiling(sqrt(n)) + K_N
-  if (is.null(b_max)) b_max <- ceiling(min(3 * sqrt(n), n / 3)) # not sure where this is coming from
+  if (is.null(b_max)) b_max <- ceiling(min(3 * sqrt(n), n / 3)) #check PW for source
   if (is.null(c)) c <- stats::qnorm(0.975) # 95% CI
 
-  ## Create two vectors of length k in which we store results.
-
+  # Store results in vectors of length k.
   b_SB_star <- rep(NA, k)
   b_CB_star <- rep(NA, k)
-
-  ## Now we loop through each variable in series (i.e., column,
-  ## series[,i]).
 
   # Initialize list to hold correlogram series
   autocorrelation <- vector(mode = "list", length = k)
 
   for(i in seq_len(length.out = k)) {
 
-    ## We first obtain the autocorrelations rho(1),...,rho(M_max) (we
-    ## need to drop the first autocorrelation as it is rho(0), hence
-    ## acf[-1]). This is the default in acf [type="correlation"]. Note
-    ## that Patton uses sample correlations after dropping the first
-    ## M_max observations, while we instead use the acf to obtain
-    ## rho(k).
-
+    # Construct auto-correlations, rho(k)
     autocorrelation[[i]] <- stats::acf(
       data[, i],
       lag.max = M_max,
@@ -132,17 +119,10 @@ pwsd <- function(
         )
     }
 
-
-    ## Next we compute m_hat. The use of c*sqrt(log10(n)/n) for
-    ## critical values is given in footnote c of Politis and White
-    ## (2004, page 59), and the approach for determining m_hat is
-    ## described in footnote c.
-
+    # Set critical value for implied hypothesis test
     rho.k.critical <- c * sqrt(log10(n) / n)
 
-    ## Compute the number of insignificant runs following each rho(k),
-    ## k=1,...,M_max.
-
+    # Count number of insignificant runs for each possible k of rho(k)
     acfSignificant <- sapply(X = seq_len(length.out = (M_max - K_N + 1)),
       FUN = sigTest,
       # Parameters for sigTest
@@ -151,45 +131,37 @@ pwsd <- function(
       K_N = K_N
       )
 
-    ## If there are any values of rho(k) for which the K_N proceeding
-    ## values of rho(k+j), j=1,...,K_N are all insignificant, take the
-    ## smallest rho(k) such that this holds (see footnote c for
-    ## further details).
-
+    # Look for insignificant runs of length K_N and take the smallest k
     if(any(acfSignificant == K_N)) {
       m_hat <- min(which(acfSignificant == K_N))
     } else {
 
-      ## If no runs of length K_N are insignificant, take the smallest
-      ## value of rho(k) that is significant.
-
+      # If no runs of length K_N are insignificant, take smallest significant value
       if(any(abs(rho.k) > rho.k.critical)) {
 
         sigLags <- which(abs(rho.k) > 10)
         numSig <- length(sigLags)
 
         if(length(numSig) > 0) {
+
           m_hat <- max(sigLags)
+
         } else {
+
+          # Use 1 as default if no prior conditions are satisfied
           m_hat <- 1
+
         }
       }
     }
 
-    ## Compute M (m_hat is at least one).
-
+    # Compute M (m_hat is at least one).
     M <- ifelse(2 * m_hat > M_max, M_max, 2 * m_hat)
 
-    ## We compute b_SB_star and b_CB_star using the formulas in the above
-    ## references. Now we require the autocovariance R(k) (hence
-    ## type="covariance" in the acf call). Note that Patton uses
-    ## sample covariances after dropping the first M_max observations,
-    ## while we instead use the acf with type="covariance" to obtain
-    ## R(k). Note also that we require R(0) hence we do not drop it as
-    ## we did for rho(k) via acf(...)$acf[-1].
-
+    # Generate sequence of lags to look over
     k_seq <- seq(from = -M, to = M, by = 1)
 
+    # Calculate R(k) in PW, the covariance of the series
     R.k <- stats::ccf(data[, i], data[, i],
       lag.max = M,
       type = "covariance",
@@ -205,10 +177,7 @@ pwsd <- function(
 
   }
 
-  ## The user can choose whether they want rounded values returned or
-  ## not. b_CB_star is rounded up, b_SB_star simply rounded but both must
-  ## be positive integers.
-
+  # Round final block-lengths
   if(round == FALSE) {
 
     b_SB_star <- ifelse(b_SB_star > b_max, b_max, b_SB_star)
@@ -243,40 +212,10 @@ pwsd <- function(
 
 }
 
-
-## $Id: ppw.R,v 1.47 2008/12/12 14:52:17 jracine Exp jracine $
-
-## Original code in Matlab by A. Patton, R translation and
-## modifications by C. Parmeter and J. Racine.
-##
-## We are grateful to Andrew Patton and Dimitris Politis for their
-## assistance and feedback. Kindly report features, deficiencies, and
-## improvements to racinej@mcmaster.ca.
-##
-## The citation is A. Patton, D.N. Politis, and H. White (2008,
-## forthcoming), "CORRECTION TO `Automatic Block-Length Selection for
-## the Dependent Bootstrap' by D.N. Politis and H. White". This is
-## based on the article by Politis, D.N., and H. White (2004),
-## "Automatic block-length selection for the dependent bootstrap."
-## Econometric Reviews, vol. 23.
-##
-## INPUTS:  data, an n x k matrix.
-##
-## OUTPUTS: b.star, a 2 x k vector of optimal bootstrap block lengths
-## for the stationary bootstrap and circular bootstrap (b_SB_star,
-## b_CB_star).
-
-
-
-
 # Helper Functions --------------------------------------------------------
 
-## The function lambda() is used to construct a "flat-top" lag window for
-## spectral estimation based on Politis, D.N. and J.P. Romano (1995),
-## "Bias-Corrected Nonparametric Spectral Estimation", Journal of Time
-## Series Analysis, vol. 16, No. 1.
-
 # Function to construct "flat-top" lag window for spectral density estimation
+## Based on Politis, D.N. and J.P. Romano (1995)
 lambda <- function(t) {
   return((abs(t) >= 0) * (abs(t) < 0.5) + 2 *
       (1 - abs(t)) * (abs(t) >= 0.5) * (abs(t)<=1))
@@ -286,12 +225,3 @@ lambda <- function(t) {
 sigTest <- function(j, rho.k, rho.k.critical, K_N) {
   sum((abs(rho.k) < rho.k.critical)[j:(j + K_N - 1)])
 }
-
-# ALEC NOTES:
-
-# M is the lag-length for the autocorrelation function ,rho.k, which is used to estimate the m_hat = M / 2. An 'implied significance' test is used to find the significant autocorrelations up to m_max (set by Politis formulas)
-
-
-# rho.k is the autocorrelation and R.k is the autocovariance!
-
-# g_hat is the spectral density function
