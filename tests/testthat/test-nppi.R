17c1ecac22859a5fd7f30bfcf471f073088f0120
Alec Stashevsky <a.stashevsky@fetchrewards.com>
2025-03-08 12:25:13 -0800

    refactor NPPI tests

--- tests/testthat/test-nppi.R
+++ tests/testthat/test-nppi.R
@@ -1,7 +1,11 @@
-test_that("nppi function returns expected structure", {
-  set.seed(42)
-  sim_data <- stats::arima.sim(list(order = c(1, 0, 0), ar = 0.5), n = 200)
+# Common Data -------------------------------------------------------------
+set.seed(42)
+sim_data <- stats::arima.sim(list(order = c(1, 0, 0), ar = 0.5), n = 200)
+small_data <- rnorm(10)  # Very small dataset
 
+
+# Tests -------------------------------------------------------------------
+test_that("nppi function returns expected structure", {
   result <- nppi(data = sim_data, stat_function = mean, num_bootstrap = 100)
 
   expect_type(result, "list")  # Ensure it's a list
@@ -21,20 +25,17 @@ test_that("nppi function returns expected structure", {
 })
 
 test_that("nppi stops execution when NA values are present", {
-  set.seed(42)
-  sim_data <- stats::arima.sim(list(order = c(1, 0, 0), ar = 0.5), n = 200)
-  sim_data[50] <- NA  # Introduce an NA value
+  # Copy the data and introduce an NA value
+  sim_data_copy <- sim_data + 0L
+  sim_data_copy[50] <- NA  # Introduce an NA value
 
   expect_error(
-    nppi(data = sim_data, stat_function = mean, num_bootstrap = 100),
+    nppi(data = sim_data_copy, stat_function = mean, num_bootstrap = 100),
     "Error: NA values detected in the time series. Please impute or remove them manually."
   )
 })
 
 test_that("nppi handles very small datasets", {
-  set.seed(42)
-  small_data <- rnorm(10)  # Very small dataset
-
   result <- nppi(data = small_data, stat_function = mean, num_bootstrap = 100)
 
   expect_gt(result$optimal_block_length, 0)
@@ -42,10 +43,7 @@ test_that("nppi handles very small datasets", {
 })
 
 test_that("nppi handles data.frame", {
-  set.seed(42)
-  small_data <- rnorm(10)
   df <- data.frame(x = small_data)
-
   result <- nppi(data = df, stat_function = mean, num_bootstrap = 100)
 
   expect_gt(result$optimal_block_length, 0)
@@ -53,9 +51,6 @@ test_that("nppi handles data.frame", {
 })
 
 test_that("nppi handles ts object", {
-  set.seed(42)
-  small_data <- rnorm(10)
-
   # Coerce to ts
   ts_data <- ts(small_data)
   result <- nppi(data = ts_data, stat_function = mean, num_bootstrap = 100)
@@ -66,11 +61,14 @@ test_that("nppi handles ts object", {
 
 
 test_that("nppi correctly sets default values for l and m", {
-  set.seed(42)
-  sim_data <- stats::arima.sim(list(order = c(1, 0, 0), ar = 0.5), n = 200)
-
   result <- nppi(data = sim_data, stat_function = mean, num_bootstrap = 100)
 
   expect_gt(result$l, 0)  # `l` should be a positive number
   expect_gt(result$m, 0)  # `m` should be a positive number
+})
+
+test_that("nppi jab_point_values and jab_pseudo_values are of the same length", {
+  result <- nppi(data = sim_data, stat_function = mean, num_bootstrap = 100)
+
+  expect_equal(length(result$jab_point_values), length(result$jab_pseudo_values))
 })
